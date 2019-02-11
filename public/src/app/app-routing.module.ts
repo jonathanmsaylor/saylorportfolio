0d6105f8981876bc3133b6a76110846090f3dee7
jonathan saylor <mashsaylor@hotmail.com>
2019-02-11 11:45:30 -0800

    home page

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { ResumePageComponent } from './resume-page/resume-page.component';
+import { SkillsPageComponent } from './skills-page/skills-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', pathMatch:'full', component: HomePageComponent},
+  {path: 'projects', component: ProjectsPageComponent},
+  {path: 'skills', component: SkillsPageComponent},
+  {path: 'resume', component: ResumePageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
