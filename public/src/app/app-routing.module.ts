dc9615d50d231c0a69127fa65144553066659b8f
jonathan saylor <mashsaylor@hotmail.com>
2019-02-11 12:57:28 -0800

    art page

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { ProjectsPageComponent } from './projects-page/projects-page.component';
 import { ResumePageComponent } from './resume-page/resume-page.component';
 import { SkillsPageComponent } from './skills-page/skills-page.component';
+import { ArtPageComponent } from './art-page/art-page.component';
+
 
 
 const routes: Routes = [
@@ -12,6 +14,7 @@ const routes: Routes = [
   {path: 'projects', component: ProjectsPageComponent},
   {path: 'skills', component: SkillsPageComponent},
   {path: 'resume', component: ResumePageComponent},
+  {path: 'art', component: ArtPageComponent},
 ];
 
 @NgModule({
