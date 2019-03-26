af19ca3b4900bbea5e3c33022a6ccb979f0f3087
Jonathan Saylor <mashsaylor@hotmail.com>
2019-03-26 12:42:20 -0700

    new resume pic and new projects / straysnewhomes

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/public/dist/public'));
+app.use(express.static(__dirname + '/dist/public'));
 
 app.get('/', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/public/dist/public/index.html'));
+res.sendFile(path.join(__dirname+'/dist/public/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
