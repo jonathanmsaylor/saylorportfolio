c5aec40686ea309df97aa49a07df46c992760a26
Jonathan Saylor <mashsaylor@hotmail.com>
2019-03-27 13:58:48 -0700

    QOL changes

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +21,8 @@ import { ProjectsPageComponent } from './projects-page/projects-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
